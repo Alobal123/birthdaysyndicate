c5e32ccd750a40f03a4c43e08d0fec6a51b5825a
mkrabec <miroslavkrabec@seznam.cz>
2026-06-10 22:26:17 +0200

    add fbuild to config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   envPrefix: ["VITE_", "NEXT_PUBLIC_"],
+  build: {
+    outDir: "fbuild",
+  },
   server: {
     port: 5173,
     host: true,
